8f9615a339af701245989b81029c84260917d795
DESKTOP-PFQ39KQ\TIANYUAN <guntertien@gmail.com>
2018-08-07 00:19:16 +0800

    格式化代码

--- code/SmartCar.c
+++ code/SmartCar.c
@@ -26,15 +26,14 @@
 
 sbit LED      =  P5 ^ 5;         		  // LED灯
 sbit LOUND    =  P5 ^ 4;           		// 蜂鸣器
-		
-sbit Servo    =  P1 ^ 4;             		//舵机转向控制 定时器0的时钟输出口
+sbit Servo    =  P1 ^ 4;             	//舵机转向控制 定时器0的时钟输出口
 sbit MOTORPWM =  P1 ^ 5;           		// 控制电机PWM 转速
-sbit MOTORIN1 =  P1 ^ 6;          		 // 控制电机方向
-sbit MOTORIN2 =  P1 ^ 7;          		 // 控制电机方向
+sbit MOTORIN1 =  P1 ^ 6;          		// 控制电机方向
+sbit MOTORIN2 =  P1 ^ 7;          	  // 控制电机方向
 
 /*注：在进行正反转切换的时候最好先刹车0.1S以上再反转，否则有可能损坏驱动器。在PWM为100%时，如果要切换电机方向，必须先刹车0.1S以上再给反转信号。*/
 
-bit busy;
+bit	B_TX1_Busy;	//发送忙标志
 bit MOTORRUNING = 1;
 
 typedef unsigned char BYTE;
@@ -94,25 +93,22 @@ void Timer2_Update(uint32 us);
 
 void main()
 {
-	P0M1 = 0;	P0M0 = 0;	//设置为准双向口
-	P1M1 = 0;	P1M0 = 0;	//设置为准双向口
-	P2M1 = 0;	P2M0 = 0;	//设置为准双向口
-	P3M1 = 0;	P3M0 = 0;	//设置为准双向口
-	P4M1 = 0;	P4M0 = 0;	//设置为准双向口
-	P5M1 = 0;	P5M0 = 0;	//设置为准双向口
-	P6M1 = 0;	P6M0 = 0;	//设置为准双向口
-	P7M1 = 0;	P7M0 = 0;	//设置为准双向口
-	
-	P1M1 &= ~(0x30);	//P1.5 P1.4 设置为推挽输出
-	P1M0 |=  (0x30);
-	
+		P0M1 = 0;	P0M0 = 0;	//设置为准双向口
+		P1M1 = 0;	P1M0 = 0;	//设置为准双向口
+		P2M1 = 0;	P2M0 = 0;	//设置为准双向口
+		P3M1 = 0;	P3M0 = 0;	//设置为准双向口
+		P4M1 = 0;	P4M0 = 0;	//设置为准双向口
+		P5M1 = 0;	P5M0 = 0;	//设置为准双向口
+		P6M1 = 0;	P6M0 = 0;	//设置为准双向口
+		P7M1 = 0;	P7M0 = 0;	//设置为准双向口
+		
+		P1M1 &= ~(0x30);	  //P1.5 P1.4 设置为推挽输出
+		P1M0 |=  (0x30);
+		
     Device_Init();
     USART_Init();
-		
 		Timer0_Init();
-
     Timer2_Init();
-
 
 	  WDT_CONTR = 0x06;       //看门狗定时器溢出时间计算公式: (12 * 32768 * PS) / FOSC (秒)
                             //设置看门狗定时器分频数为32,溢出时间如下:
@@ -129,25 +125,25 @@ void main()
 
 void Timer0_Init(void)		//@11.0592MHz
 {
-	AUXR |= 0x80;		//定时器时钟1T模式
-	TMOD &= 0xF0;		//设置定时器模式
-	TMOD |= 0x01;		//设置定时器模式
-  TL0 = 0x33;			//设置定时初值
-	TH0 = 0xBF;			//设置定时初值
-	TF0 = 0;				//清除TF0标志
-	TR0 = 1;				//定时器0开始计时
-	ET0 = 1;        //使能定时器0中断
-  EA = 1;
+		AUXR |= 0x80;		//定时器时钟1T模式
+		TMOD &= 0xF0;		//设置定时器模式
+		TMOD |= 0x01;		//设置定时器模式
+		TL0 = 0x33;			//设置定时初值
+		TH0 = 0xBF;			//设置定时初值
+		TF0 = 0;				//清除TF0标志
+		TR0 = 1;				//定时器0开始计时
+		ET0 = 1;        //使能定时器0中断
+		EA = 1;
 }
 
 
 void Timer0_Update(uint32 us)	 	
 {	
-	uint32 valu;
-	valu=0xffff-us;  
-  TH0=valu>>8;   	
-  TL0=valu;
-  TR0  = 1;				//T0开始工作
+		uint32 valu;
+		valu = 0xffff-us;  
+		TH0  = valu>>8;   	
+		TL0  = valu;
+		TR0  = 1;				//T0开始工作
 }
 
 
@@ -155,81 +151,79 @@ void Timer0_Update(uint32 us)
 //定时器0中断服务程序
 void Timer0_interrupt() interrupt 1
 {
-	static unsigned char   i=1;
+		static unsigned char   i=1;
 
-	switch(i)	  
-	{
-			case 1:
-			{
-				Servo = 1;
-				
-				Timer0_Update(PWMHEIGHT);
-			}  break;
-			case 2:
-			{
-			 	Servo=0;     //	pwm1变低 
-				
-				Timer0_Update(0x6BFF - PWMHEIGHT);
-			}  break;
-			case 3:
-			{
-				Timer0_Update(0x6BFF);
-			}  break;
-			case 4:
-			{
-				Timer0_Update(0x6BFF);
-			}  break;
-			case 5:
-			{
-				Timer0_Update(0x6BFF);
-			}  break;
-			case 6:
-			{
-				Timer0_Update(0x6BFF);
-			}  break;
-			case 7:
-			{
-				Timer0_Update(0x6BFF);
-			}  break;
-			case 8:
-			{
-				Timer0_Update(0x6BFF);
-			}  break;
-			case 9:
-			{
-				Timer0_Update(0x6BFF);
-        i=0;
-			}  break;
+		switch(i)	  
+		{
+				case 1:
+				{
+					Servo = 1;
+					
+					Timer0_Update(PWMHEIGHT);
+				}  break;
+				case 2:
+				{
+					Servo=0;     //	pwm1变低 
+					
+					Timer0_Update(0x6BFF - PWMHEIGHT);
+				}  break;
+				case 3:
+				{
+					Timer0_Update(0x6BFF);
+				}  break;
+				case 4:
+				{
+					Timer0_Update(0x6BFF);
+				}  break;
+				case 5:
+				{
+					Timer0_Update(0x6BFF);
+				}  break;
+				case 6:
+				{
+					Timer0_Update(0x6BFF);
+				}  break;
+				case 7:
+				{
+					Timer0_Update(0x6BFF);
+				}  break;
+				case 8:
+				{
+					Timer0_Update(0x6BFF);
+				}  break;
+				case 9:
+				{
+					Timer0_Update(0x6BFF);
+					i=0;
+				}  break;
 
-			default:break;
-	}
-	i++;
-    
+				default:break;
+		}
+		i++;
+			
 }
 
 
 void Timer2_Init(void)		//@11.0592MHz  
 {
-	AUXR |= 0x04;		//定时器时钟1T模式
-	T2L = 0xEC;		//设置定时初值
-	T2H = 0xEF;		//设置定时初值
-	AUXR |= 0x10;		//定时器2开始计时
-	IE2  |=  (1<<2);	//允许中断
+		AUXR |= 0x04;		//定时器时钟1T模式
+		T2L   = 0xEC;		//设置定时初值
+		T2H   = 0xEF;		//设置定时初值
+		AUXR |= 0x10;		//定时器2开始计时
+		IE2  |=  (1<<2);	//允许中断
 
 }
-
 
 
 void Timer2_Update(uint32 us)	 	
 {	
 
-	uint32 valu;
-	IE2  |=  (0<<2);	//允许中断
-
-	valu=0xffff-us;  
-  T2H=valu>>8;   	
-  T2L=valu;
-	IE2  |=  (1<<2);	//允许中断
+		uint32 valu;
+		IE2  |=  (0<<2);	//允许中断
+		valu  = 0xffff-us;  
+		T2H   = valu>>8;   	
+		T2L   = valu;
+		IE2  |=  (1<<2);	//允许中断
 }
 
 
@@ -237,57 +231,56 @@ void Timer2_Update(uint32 us)
 //定时器2中断服务程序
 void timer2_interrupt (void) interrupt 12
 {
-	static unsigned char   ss = 1;
+		static unsigned char   ss = 1;
 
-	switch(ss)
-	{
-			case 1:
-			{
-				MOTORPWM =1;
-				
-				Timer2_Update(MOTORDUTY);
-			}  break;
-			case 2:
-			{
-				MOTORPWM = 0;
-				
-				Timer2_Update(0x6BFF - MOTORDUTY);
-			}  break;
-			case 3:
-			{
-				Timer2_Update(0x6BFF);
-			}  break;
-			case 4:
-			{
-				Timer2_Update(0x6BFF);
-			}  break;
-			case 5:
-			{
-				Timer2_Update(0x6BFF);
-			}  break;
-			case 6:
-			{
-				Timer2_Update(0x6BFF);
-			}  break;
-			case 7:
-			{
-				Timer2_Update(0x6BFF);
-			}  break;
-			case 8:
-			{
-				Timer2_Update(0x6BFF);
-			}  break;
-			case 9:
-			{
-				Timer2_Update(0x6BFF);
+		switch(ss)
+		{
+				case 1:
+				{
+					MOTORPWM =1;
+					
+					Timer2_Update(MOTORDUTY);
+				}  break;
+				case 2:
+				{
+					MOTORPWM = 0;
+					
+					Timer2_Update(0x6BFF - MOTORDUTY);
+				}  break;
+				case 3:
+				{
+					Timer2_Update(0x6BFF);
+				}  break;
+				case 4:
+				{
+					Timer2_Update(0x6BFF);
+				}  break;
+				case 5:
+				{
+					Timer2_Update(0x6BFF);
+				}  break;
+				case 6:
+				{
+					Timer2_Update(0x6BFF);
+				}  break;
+				case 7:
+				{
+					Timer2_Update(0x6BFF);
+				}  break;
+				case 8:
+				{
+					Timer2_Update(0x6BFF);
+				}  break;
+				case 9:
+				{
+					Timer2_Update(0x6BFF);
 
-        ss=0;
-			}  break;
+					ss=0;
+				}  break;
 
-			default:break;
-	}
-	ss++;
-    
+				default:break;
+		}
+		ss++;
 }
 
 
@@ -329,10 +322,13 @@ void USART_Init()
     TL1 = (65536 - (FOSC/4/BAUD));   //设置波特率重装值
     TH1 = (65536 - (FOSC/4/BAUD))>>8;
     TR1 = 1;                    //定时器1开始启动
-		PS = 1;                     //串口1中断优先级最高，以防出现无法控制情况
-	//	PT1 = 1;                     //定时器1中断优先级控制位
+	//	PS = 1;                     //串口1中断优先级最高，以防出现无法控制情况
+	//  PT1 = 1;                     //定时器1中断优先级控制位
     ES = 1;                     //使能串口中断
     EA = 1;
+		
+		B_TX1_Busy = 0;
+
 
 }
 
@@ -346,14 +342,12 @@ void Uart_interrupt() interrupt 4 using 1
         while(!RI);
         RI=0;
         UART_R();
-        busy = 0;
 
     }
     if (TI)
     {
         while(!TI);
         TI = 0;                 //清除TI位
-        busy = 0;               //清忙标志
     }
 }
 
@@ -397,9 +391,10 @@ void UART_R()
 {
     DATA_GET[CURRENT_LENGTH]=SBUF;
     CURRENT_LENGTH++;
-    if(CURRENT_LENGTH==DATA_LENGTH)
+    if(CURRENT_LENGTH==DATA_LENGTH && !B_TX1_Busy)
     {
         CURRENT_LENGTH=0;
+				B_TX1_Busy = 1;
         ResponseData(DATA_GET);
     }
 }
@@ -416,163 +411,164 @@ char CheckData(unsigned char *CHECK_DATA) {
 
 
 void ResponseData(unsigned char *RES_DATA) {
-	if(CheckData(RES_DATA) == RES_DATA[5]) {
-			switch(RES_DATA[1]){
-				case 0x01:{//转弯和角度
-					if( 0x00<=RES_DATA[3]<=0x02 && 0x00<=RES_DATA[4]<=0x03){
-            sendAckData(RES_DATA);
-						
-						//1.0ms 0X2B32 向左45度角 3挡
-						//1. ms 0X3265 向左30度角 2挡 
-						//1. ms 0X3998 向左15度角 1挡
-						//1.5ms 0X40CC 居中
-						//1. ms 0X47FE 向右15度角 1挡
-						//1. ms 0X4F31 向右30度角 2挡 
-						//2.0ms 0X5665 向右45度角 3挡
-						
-						if(RES_DATA[3] == 0x00 || RES_DATA[4] == 0x00 ){//居中
-								PWMHEIGHT = 0X40CC;
-						  }else if(RES_DATA[3] == 0x02){//左转
-							if(RES_DATA[4] == 0x01){
-								PWMHEIGHT = 0X3998;
-						  }else if(RES_DATA[4] == 0x02){
-								PWMHEIGHT = 0X3265;
-						  }else if(0x03 <= RES_DATA[4] ){
-								PWMHEIGHT = 0X2B32;
-						  }
-						}else if(RES_DATA[3] == 0x01){//右转
-							if(RES_DATA[4] == 0x01){
-								PWMHEIGHT = 0X47FE;
-						  }else if(RES_DATA[4] == 0x02){
-								PWMHEIGHT = 0X4F31;
-						  }else if(0x03 <= RES_DATA[4] ){
-								PWMHEIGHT = 0X5665;
-						  }
-						}
-						
-						
-					}
-					break;
-			  };
-				case 0x02:{//喇叭
-					if( RES_DATA[4]==0x02){
-						 LOUND = 1;
-            sendAckData(RES_DATA);
-					}else if( RES_DATA[4]==0x01){
-						LOUND = 0;
-            sendAckData(RES_DATA);
-					}
-					break;
-			  };
-				case 0x03:{//灯
-					if( RES_DATA[4]==0x02){
-						LED = 1;
-            sendAckData(RES_DATA);
-					}else if( RES_DATA[4]==0x01){
-						LED = 0;
-            sendAckData(RES_DATA);
-					}
-					break;
-			  };
-				case 0x04:{//方向和油门
-					if( 0x00<=RES_DATA[3]<=0x02 && 0x00<=RES_DATA[4]<=0x03 && MOTORRUNING == 1){
-            sendAckData(RES_DATA);
-						
-						
-							if(RES_DATA[3] == 0x00 || RES_DATA[4] == 0x00 ){//停止
-										MOTORIN1=0;
-										MOTORIN2=0;
-/*注：在进行正反转切换的时候最好先刹车0.1S以上再反转，否则有可能损坏驱动器。
-在PWM为100%时，如果要切换电机方向，必须先刹车0.1S以上再给反转信号。*/
-
-										MOTORRUNING = 0;
-					        	DELAY_MS(200);
-										MOTORRUNING = 1;
-										
-						  }else if(RES_DATA[3] == 0x02){//前进
-												MOTORIN1=1;
-										    MOTORIN2=0;	
-												
-												
-									if(RES_DATA[4] == 0x01){
-													MOTORDUTY = 0X0115;
-									}else if(RES_DATA[4] == 0x02){
-													MOTORDUTY = 0X1033;
-									}else if(0x03 <= RES_DATA[4] ){
-													MOTORDUTY = 0X5663;
-									}
-									
-						}else if(RES_DATA[3] == 0x01){//后退
-								MOTORIN1=0;
-								MOTORIN2=1;	
-												
-									if(RES_DATA[4] == 0x01){
-													MOTORDUTY = 0X0115;
-									}else if(RES_DATA[4] == 0x02){
-													MOTORDUTY = 0X1033;
-									}else if(0x03 <= RES_DATA[4] ){
-													MOTORDUTY = 0X5663;
-									}
+		if(CheckData(RES_DATA) == RES_DATA[5]) {
+				switch(RES_DATA[1]){
+					case 0x01:{//转弯和角度
+						if( 0x00<=RES_DATA[3]<=0x02 && 0x00<=RES_DATA[4]<=0x03){
+							sendAckData(RES_DATA);
+							
+							//1.0ms 0X2B32 向左45度角 3挡
+							//1. ms 0X3265 向左30度角 2挡 
+							//1. ms 0X3998 向左15度角 1挡
+							//1.5ms 0X40CC 居中
+							//1. ms 0X47FE 向右15度角 1挡
+							//1. ms 0X4F31 向右30度角 2挡 
+							//2.0ms 0X5665 向右45度角 3挡
+							
+							if(RES_DATA[3] == 0x00 || RES_DATA[4] == 0x00 ){//居中
+									PWMHEIGHT = 0X40CC;
+								}else if(RES_DATA[3] == 0x02){//左转
+								if(RES_DATA[4] == 0x01){
+									PWMHEIGHT = 0X3998;
+								}else if(RES_DATA[4] == 0x02){
+									PWMHEIGHT = 0X3265;
+								}else if(0x03 <= RES_DATA[4] ){
+									PWMHEIGHT = 0X2B32;
+								}
+							}else if(RES_DATA[3] == 0x01){//右转
+								if(RES_DATA[4] == 0x01){
+									PWMHEIGHT = 0X47FE;
+								}else if(RES_DATA[4] == 0x02){
+									PWMHEIGHT = 0X4F31;
+								}else if(0x03 <= RES_DATA[4] ){
+									PWMHEIGHT = 0X5665;
+								}
+							}
+							
 							
 						}
-						
-					}
-					break;
-			  };
-				case 0x05:{//寻车
-					if( RES_DATA[4]==0x02){
-						sendAckData(RES_DATA);
-						LED = 1;
-						LOUND = 1;
-						DELAY_MS(200);
-						LED = 0;
-						LOUND = 0;
-						DELAY_MS(200);
-						LED = 1;
-						LOUND = 1;
-						DELAY_MS(200);
-						LED = 0;
-						LOUND = 0;
-						DELAY_MS(200);
-						LED = 1;
-						LOUND = 1;
-						DELAY_MS(200);
-						LED = 0;
-						LOUND = 0;
-						DELAY_MS(200);
-					}
-					break;
-			  };
-				case 0x06:{//引擎   TODOLED显示工作状态
-					if( RES_DATA[4]==0x02){
-						LED = 1;
-						LOUND = 1;
-						DELAY_MS(200);
-						LED = 0;
-						LOUND = 0;
-            sendAckData(RES_DATA);
-					}else if( RES_DATA[4]==0x01){
-						LED = 1;
-						LOUND = 1;
-						DELAY_MS(200);
-						LED = 0;
-						LOUND = 0;
-            sendAckData(RES_DATA);
-					}
-					break;
-			  };
-				
-				default:
-					break;
-				
+						break;
+					};
+					case 0x02:{//喇叭
+						if( RES_DATA[4]==0x02){
+							 LOUND = 1;
+							sendAckData(RES_DATA);
+						}else if( RES_DATA[4]==0x01){
+							LOUND = 0;
+							sendAckData(RES_DATA);
+						}
+						break;
+					};
+					case 0x03:{//灯
+						if( RES_DATA[4]==0x02){
+							LED = 1;
+							sendAckData(RES_DATA);
+						}else if( RES_DATA[4]==0x01){
+							LED = 0;
+							sendAckData(RES_DATA);
+						}
+						break;
+					};
+					case 0x04:{//方向和油门
+						if( 0x00<=RES_DATA[3]<=0x02 && 0x00<=RES_DATA[4]<=0x03 && MOTORRUNING == 1){
+							sendAckData(RES_DATA);
+							
+							
+								if(RES_DATA[3] == 0x00 || RES_DATA[4] == 0x00 ){//停止
+											MOTORIN1=0;
+											MOTORIN2=0;
+	/*注：在进行正反转切换的时候最好先刹车0.1S以上再反转，否则有可能损坏驱动器。
+	在PWM为100%时，如果要切换电机方向，必须先刹车0.1S以上再给反转信号。*/
+
+											MOTORRUNING = 0;
+											DELAY_MS(200);
+											MOTORRUNING = 1;
+											
+								}else if(RES_DATA[3] == 0x02){//前进
+													MOTORIN1=1;
+													MOTORIN2=0;	
+													
+													
+										if(RES_DATA[4] == 0x01){
+														MOTORDUTY = 0X0115;
+										}else if(RES_DATA[4] == 0x02){
+														MOTORDUTY = 0X1033;
+										}else if(0x03 <= RES_DATA[4] ){
+														MOTORDUTY = 0X5663;
+										}
+										
+							}else if(RES_DATA[3] == 0x01){//后退
+									MOTORIN1=0;
+									MOTORIN2=1;	
+													
+										if(RES_DATA[4] == 0x01){
+														MOTORDUTY = 0X0115;
+										}else if(RES_DATA[4] == 0x02){
+														MOTORDUTY = 0X1033;
+										}else if(0x03 <= RES_DATA[4] ){
+														MOTORDUTY = 0X5663;
+										}
+								
+							}
+							
+						}
+						break;
+					};
+					case 0x05:{//寻车
+						if( RES_DATA[4]==0x02){
+							sendAckData(RES_DATA);
+							LED = 1;
+							LOUND = 1;
+							DELAY_MS(200);
+							LED = 0;
+							LOUND = 0;
+							DELAY_MS(200);
+							LED = 1;
+							LOUND = 1;
+							DELAY_MS(200);
+							LED = 0;
+							LOUND = 0;
+							DELAY_MS(200);
+							LED = 1;
+							LOUND = 1;
+							DELAY_MS(200);
+							LED = 0;
+							LOUND = 0;
+							DELAY_MS(200);
+						}
+						break;
+					};
+					case 0x06:{//引擎   TODOLED显示工作状态
+						if( RES_DATA[4]==0x02){
+							LED = 1;
+							LOUND = 1;
+							DELAY_MS(200);
+							LED = 0;
+							LOUND = 0;
+							sendAckData(RES_DATA);
+						}else if( RES_DATA[4]==0x01){
+							LED = 1;
+							LOUND = 1;
+							DELAY_MS(200);
+							LED = 0;
+							LOUND = 0;
+							sendAckData(RES_DATA);
+						}
+						break;
+					};
+					
+					default:
+						break;
+					
+				}
 			}
-    }
+		
+		B_TX1_Busy = 0;
 
 }
 
 
 void sendAckData(unsigned char *RES_DATA) {
-
 
     unsigned char DATA_SEND[]= { 0x7E, 0x00,     0x02,  0x00,    0x00 ,      0x00,       0x7E};
 
@@ -597,8 +593,8 @@ void sendAckData(unsigned char *RES_DATA) {
 //========================================================================
 void  DELAY_MS(uint8 ms)
 {
-     unsigned int i;
-	 do{
+    unsigned int i;
+		do{
 	      i = MAIN_Fosc / 13000;
 		  while(--i)	;
      }while(--ms);
